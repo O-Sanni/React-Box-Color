08a8fcf6ae27905e38185fbe0e2e6ea1ae16659c
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-26 11:58:49 -0400

    add random colors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      color: "coral",
+      colorText: "purple",
+      clickCounter: 0
+    }
+  }
+  onChange=()=>{
+   const colors=["purple","red","green","darkorchid", "aqua","cyan","darkkhaki", "gold", "yellow","chartreuse","burlywood","aliceblue", "orange","blue","brown","antiquewhite","aquamarine","coral","darkviolet"];
+    let randomColor=colors[Math.floor(Math.random()*colors.length)];
+    let randomTextColor=colors[Math.floor(Math.random()*colors.length)];
+    this.setState({
+      color: randomColor,
+      colorText:randomTextColor,
+      clickCounter: (this.state.clickCounter+1)
+    })
+    console.log("click counter ===>",this.state.clickCounter)
+  }
+  render(){
+    return (
+      <div className="App">
+      <h1>Box color change</h1>
+      <div className="colorBox"
+      style={{backgroundColor:
+      this.state.color}}
+      onClick={this.onChange}>
+        <p style={{color:this.state.colorText}}>Click Me
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+      </div>
+    )
+  }
 }
 
 export default App;
